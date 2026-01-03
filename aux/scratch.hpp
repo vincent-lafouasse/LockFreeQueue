bbe17cb175fdc164e8193f8f18475b67c7db3829
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-04 00:34:23 +0100

    move buffer to the heap

--- aux/scratch.hpp
+++ aux/scratch.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
-#include <array>
 #include <atomic>
 #include <cassert>
 #include <cstddef>
+#include <memory>
 
 // spsc
-template <class T, size_t N>
+template <class T>
 class LockFreeQueue {
    public:
-    LockFreeQueue() : size_(0), read_pos_(0), write_pos_(0)
+    LockFreeQueue(size_t cap)
+        : cap_(cap), mask_(cap - 1), size_(0), read_pos_(0), write_pos_(0)
     {
-        static_assert(N > 0 && (N & (N - 1)) == 0, "N must be a power of 2");
         static_assert(size_.is_always_lock_free,
                       "cannot guarantee size_t is always lock-free");  // C++17
         assert(size_.is_lock_free() &&
                "size_t is not lock-free on this platform");
+        assert(cap > 0 && (cap & (cap - 1)) == 0 && "N must be a power of 2");
+        buffer_ = std::make_unique<T[]>(cap);
     }
 
     // shared API
@@ -24,12 +26,11 @@ class LockFreeQueue {
     // writer thread API
     bool push(const T& t)
     {
-        if (size_.load() >= N) {
+        if (size_.load() >= cap_) {
             return false;
         }
         buffer_[write_pos_] = t;
-        write_pos_ =
-            (write_pos_ + 1) & (N - 1);  // fast and correct is N is a pow2
+        write_pos_ = (write_pos_ + 1) & mask_;
         size_.fetch_add(1);
         return true;
     }
@@ -50,13 +51,16 @@ class LockFreeQueue {
         if (s == 0) {
             return false;
         }
-        read_pos_ = (read_pos_ + 1) & (N - 1);
+        read_pos_ = (read_pos_ + 1) & mask_;
         size_.fetch_sub(1);
         return true;
     }
 
    private:
-    std::array<T, N> buffer_{};
+    const size_t cap_;
+    const size_t mask_;
+    std::unique_ptr<T[]> buffer_;
+
     std::atomic<size_t> size_{};
     size_t read_pos_ = 0;   // not shared between threads
     size_t write_pos_ = 0;  // not shared between threads

088fba9fa4e96f5ed54af48807d010afb958d611
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-07 13:28:08 +0100

    implement Lamport queue

---
+++ aux/Lamport.cpp
@@ -0,0 +1,43 @@
+#include <array>
+#include <atomic>
+#include <cstddef>
+
+namespace Lamport {
+using Data = float;
+
+constexpr size_t Size = 1024;  // number of elements
+
+static std::array<Data, Size> Q{};
+
+static std::atomic<size_t> Head{0};
+static std::atomic<size_t> Tail{0};
+
+// assumes single producer
+void put(Data value)
+{
+    const size_t tail = Tail.load(std::memory_order_acquire);
+
+    // spin while waiting for there to be space ??
+    while (tail - Head.load(std::memory_order_acquire) == Size) {
+    }
+
+    Q[tail] = value;
+    const size_t new_tail = (tail + 1) % Size;
+    Tail.store(new_tail, std::memory_order_release);
+}
+
+// assume single consumer
+Data get()
+{
+    const size_t head = Head.load(std::memory_order_acquire);
+
+    // spin while waiting for there to be anything in there ??
+    while (head == Tail.load(std::memory_order_acquire)) {
+    }
+
+    const Data out = Q[head];
+    const size_t new_head = (head + 1) % Size;
+    Head.store(new_head, std::memory_order_release);
+    return out;
+}
+}  // namespace Lamport

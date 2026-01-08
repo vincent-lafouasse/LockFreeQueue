241e0ea57f7fa16726b0ada522e1a86ffba5e514
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-08 14:43:15 +0100

    parse authors

--- aux/citation_formatter.py
+++ aux/citation_formatter.py
@@ -1,8 +1,22 @@
 import bibtexparser
+import re
 
 with open("references.bib") as bibtex_file:
     bib_database = bibtexparser.load(bibtex_file)
 
+parsed_data = []
+
 for entry in bib_database.entries:
-    print(f"Title: {entry['title']}")
-    print(f"Authors: {entry['author']}")
+    raw_authors = entry.get("author", "")
+    author_list = re.split(r"\s+and\s+", raw_authors, flags=re.IGNORECASE)
+    author_list = [author.strip() for author in author_list]
+    parsed_data.append(
+        {"title": entry.get("title", "Unknown Title"), "authors": author_list}
+    )
+
+for item in parsed_data:
+    print(f"Title:\n    {item['title']}")
+    print(f"Authors:")
+    for author in item['authors']:
+        print(f"    {author}")
+    print()

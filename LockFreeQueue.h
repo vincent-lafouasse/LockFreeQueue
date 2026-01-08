6693554ccc8c701da05638ff34c7336200073c66
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-08 22:34:46 +0100

    manually pad to be absolutely sure of memory layout

--- LockFreeQueue.h
+++ LockFreeQueue.h
@@ -37,7 +37,9 @@ typedef struct LockFreeQueue LockFreeQueue;
 struct LockFreeQueue {
     // avoid false sharing
     alignas(CACHE_LINE) _Atomic(size_t) front;
+    char pad1[CACHE_LINE - sizeof(_Atomic(size_t))];
     alignas(CACHE_LINE) _Atomic(size_t) back;
+    char pad2[CACHE_LINE - sizeof(_Atomic(size_t))];
     alignas(CACHE_LINE) float data[CLF_QUEUE_SIZE];
 };
 

3f9553e0c637bf9030401a1312288e1ba7aff5b2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-01-08 12:53:32 +0100

    refactor push operation using lazy then eager size check

--- LockFreeQueue.c
+++ LockFreeQueue.c
@@ -42,23 +42,16 @@ size_t clfq_producer_size_eager(LockFreeQueueProducer* producer)
 
 bool clfq_push(LockFreeQueueProducer* producer, const float* elems, size_t n)
 {
+    // check pessimistically, if it's fine do not reload `front`
+    // we consider relaxed loading of private `back` free
+    if (clfq_producer_size_lazy(producer) < n &&
+        clfq_producer_size_eager(producer) < n) {
+        return false;
+    }
+
     // Producer is sole writer so there is no contention on `back`
     const size_t back =
         atomic_load_explicit(producer->back, memory_order_relaxed);
-
-    // if the cached front already allows enough space for the transaction, do
-    // not pessimistically load the latest front as the available size would
-    // only get bigger with a fresher `front`
-    if (distance(back, producer->cached_front + CLF_QUEUE_SIZE) < n) {
-        // else we need to reload `front` and do this calculation again
-        producer->cached_front =
-            atomic_load_explicit(producer->front, memory_order_acquire);
-        if (distance(back, producer->cached_front + CLF_QUEUE_SIZE) < n) {
-            // 100% can't fit the whole buffer passed
-            // TODO: partial version
-            return false;
-        }
-    }
 
     for (size_t i = 0; i < n; ++i) {
         producer->data[(back + i) & (CLF_QUEUE_SIZE - 1)] = elems[i];

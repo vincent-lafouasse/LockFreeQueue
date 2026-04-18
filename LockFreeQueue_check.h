7df02c2c194f3b90c8f1703f4aec4196dace022e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-04-18 20:32:20 +0200

    check that u32 is always lock-free, implementation still uses size_t for now

--- LockFreeQueue_check.h
+++ LockFreeQueue_check.h
@@ -11,3 +11,19 @@
     "implementation"
 _Static_assert(((CLF_QUEUE_SIZE & (CLF_QUEUE_SIZE - 1)) == 0),
                INVALID_QUEUE_SIZE_MSG);
+
+#include <stdatomic.h>
+
+// it's very important that the size type we use is _always_ lock-free
+//
+// (int is i32) and (ints are always lock-free) => i32 is always lock-free
+// => u32 is always lock-free
+
+_Static_assert(sizeof(int) == 4,
+               "This codebase strictly requires 4-byte integers.");
+_Static_assert(
+    ATOMIC_INT_LOCK_FREE == 2,
+    "32-bit operations are not natively lock-free on this platform.");
+
+// now that we are assured that u32 are always lock-free
+typedef uint32_t ClfqSizeType;
